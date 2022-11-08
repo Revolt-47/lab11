39b6ea1638ccf8d1ed29d6f0be5a338138a229bd
Revolt_47 <i201806@nu.edu.pk>
2022-11-08 13:24:36 +0500

    task3done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import List from './list';
 
 function App() {
   return (
     <div data-testid = "comp" className="App">
-      <h1>We Love Kazim</h1>
+      <h1>Hello world</h1>
+      <List/>
     </div>
   );
 }
---
+++ src/list.js
@@ -0,0 +1,13 @@
+export default function List(){
+    return(
+        <div data-testid = "comp1">
+            <ul>
+                <li>Babar</li>
+                <li>Rizwan</li>
+                <li>Haris</li>
+                <li>Chacha</li>
+                <li>Shaheena</li>
+            </ul>
+        </div>
+    )
+}
